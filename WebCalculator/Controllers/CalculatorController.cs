af5daaee668734a0cfdfe7cbad55efd25fcd08f3
Sean Schwabenlander <sean@schwabenlander.com>
2017-01-12 23:34:26 -0600

    Got rid of exception handling because it turns out: According to Microsoft, "Floating-point arithmetic overflow or division by zero never throws an exception, because floating-point types are based on IEEE 754 and so have provisions for representing infinity and NaN (Not a Number)."

--- WebCalculator/Controllers/CalculatorController.cs
+++ WebCalculator/Controllers/CalculatorController.cs
@@ -13,171 +13,61 @@ namespace WebCalculator.Controllers
         [Route("api/calculator/add")]
         public IHttpActionResult Add([FromBody]EquationOperandsModel equation)
         {
-            double result;
-
-            try
-            {
-                result = equation.Operand1 + equation.Operand2;
-            }
-            catch (Exception)
-            {
-                return BadRequest("Invalid input.");
-            }
-            
-            return Ok(result);
+            return Ok(equation.Operand1 + equation.Operand2);
         }
 
         [Route("api/calculator/subtract")]
         public IHttpActionResult Subtract([FromBody]EquationOperandsModel equation)
         {
-            double result;
-
-            try
-            {
-                result = equation.Operand1 - equation.Operand2;
-            }
-            catch (Exception)
-            {
-                return BadRequest("Invalid input.");
-            }
-
-            return Ok(result);
+            return Ok(equation.Operand1 - equation.Operand2);
         }
 
         [Route("api/calculator/multiply")]
         public IHttpActionResult Multiply([FromBody]EquationOperandsModel equation)
         {
-            double result;
-
-            try
-            {
-                result = equation.Operand1 * equation.Operand2;
-            }
-            catch (Exception)
-            {
-                return BadRequest("Invalid input.");
-            }
-
-            return Ok(result);
+            return Ok(equation.Operand1 * equation.Operand2);
         }
 
         [Route("api/calculator/divide")]
         public IHttpActionResult Divide([FromBody]EquationOperandsModel equation)
         {
-            double result;
-
-            try
-            {
-                result = equation.Operand1 / equation.Operand2;
-            }
-            catch (Exception)
-            {
-                return BadRequest("Invalid input.");
-            }
-
-            return Ok(result);
+            return Ok(equation.Operand1 / equation.Operand2);
         }
 
         [Route("api/calculator/mod")]
         public IHttpActionResult Mod([FromBody]EquationOperandsModel equation)
         {
-            double result;
-
-            try
-            {
-                result = equation.Operand1 % equation.Operand2;
-            }
-            catch (Exception)
-            {
-                return BadRequest("Invalid input.");
-            }
-
-            return Ok(result);
+            return Ok(equation.Operand1 % equation.Operand2);
         }
 
         [Route("api/calculator/square")]
         public IHttpActionResult Square([FromBody]EquationOperandsModel equation)
         {
-            double result;
-
-            try
-            {
-                result = equation.Operand1 * equation.Operand1;
-            }
-            catch (Exception)
-            {
-                return BadRequest("Invalid input.");
-            }
-
-            return Ok(result);
+            return Ok(equation.Operand1 * equation.Operand1);
         }
 
         [Route("api/calculator/squareroot")]
         public IHttpActionResult SquareRoot([FromBody]EquationOperandsModel equation)
         {
-            double result;
-
-            try
-            {
-                result = Math.Sqrt(equation.Operand1);
-            }
-            catch (Exception)
-            {
-                return BadRequest("Invalid input.");
-            }
-
-            return Ok(result);
+            return Ok(Math.Sqrt(equation.Operand1));
         }
 
         [Route("api/calculator/sin")]
         public IHttpActionResult Sin([FromBody]EquationOperandsModel equation)
         {
-            double result;
-
-            try
-            {
-                result = Math.Sin(equation.Operand1);
-            }
-            catch (Exception)
-            {
-                return BadRequest("Invalid input.");
-            }
-
-            return Ok(result);
+            return Ok(Math.Sin(equation.Operand1));
         }
 
         [Route("api/calculator/cos")]
         public IHttpActionResult Cos([FromBody]EquationOperandsModel equation)
         {
-            double result;
-
-            try
-            {
-                result = Math.Cos(equation.Operand1);
-            }
-            catch (Exception)
-            {
-                return BadRequest("Invalid input.");
-            }
-
-            return Ok(result);
+            return Ok(Math.Cos(equation.Operand1));
         }
 
         [Route("api/calculator/tan")]
         public IHttpActionResult Tan([FromBody]EquationOperandsModel equation)
         {
-            double result;
-
-            try
-            {
-                result = Math.Tan(equation.Operand1);
-            }
-            catch (Exception)
-            {
-                return BadRequest("Invalid input.");
-            }
-
-            return Ok(result);
+            return Ok(Math.Tan(equation.Operand1));
         }
     }
 }
