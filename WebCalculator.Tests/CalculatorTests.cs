c2ed50b197134fbd7d42a5d50b46680fded86403
Sean Schwabenlander <sean@schwabenlander.com>
2017-01-12 22:04:19 -0600

    Initial setup.

---
+++ WebCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebCalculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Two_Plus_Two_Equals_Four()
+        {
+            int expectedResult = 4;
+
+            Assert.AreEqual(expectedResult, 2 + 2);
+        }
+    }
+}
